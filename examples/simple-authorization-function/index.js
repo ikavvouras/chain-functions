f4805a0b829602b92d7a3c7bdfb03f6b05a988b0
Yannis Kavvouras <jhkavv@gmail.com>
2021-12-04 17:01:53 +0200

    add simple-authorization-function example

---
+++ examples/simple-authorization-function/index.js
@@ -0,0 +1,24 @@
+const { simpleFunction } = require('../../index');
+const cors = require('cors');
+
+async function authorizationFilter(req, res, next) {
+  const authorizationHeader = req.headers.authorization;
+  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+    res.status(401).send({ error: 'Unauthorized' });
+    return;
+  }
+
+  const token = authorizationHeader.split('Bearer ')[1];
+  if (token !== 'my-token') {
+    res.status(403).send({ error: 'Unauthorized' });
+    return;
+  }
+
+  await next();
+}
+
+function helloWorldFunction(req, res) {
+  res.send(`Hello ${(req.query.name || req.body.name || 'World')}!`);
+}
+
+exports.helloWorld = simpleFunction(cors(), authorizationFilter, helloWorldFunction);
---
+++ test/simple-authorization-function.test.js
@@ -0,0 +1,46 @@
+const { exec } = require('child_process');
+const { request } = require('axios');
+const waitPort = require('wait-port');
+const path = require('path');
+const { expect, assert, should } = require('chai')
+
+const functionPath = path.resolve(__dirname, '../examples/simple-authorization-function')
+const PORT = 8888;
+const BASE_URL = `http://localhost:${PORT}`;
+
+describe('simple-authorization-function', () => {
+
+  before(async () => {
+    this.ffProc = exec(
+      `npx functions-framework --target=helloWorld --signature-type=http --port=${PORT} --source=${functionPath}`
+    );
+    await waitPort({ host: 'localhost', port: PORT });
+  });
+
+  after(() => this.ffProc.kill());
+
+  it('should return hello world', async () => {
+    const response = await request({
+      url: `${BASE_URL}/helloWorld`,
+      headers: {
+        'Authorization': 'Bearer my-token'
+      },
+      method: 'GET',
+    });
+
+    expect(response.status).to.equal(200);
+    expect(response.headers).to.include({ 'access-control-allow-origin': '*' });
+    expect(response.data).to.equal('Hello World!');
+  });
+
+  it('should return 401 if authorization header is missing', async () => {
+    try {
+      await request({
+        url: `${BASE_URL}/helloWorld`,
+        method: 'GET'
+      });
+    } catch (e) {
+      expect(e.response.status).to.equal(401);
+    }
+  });
+});
